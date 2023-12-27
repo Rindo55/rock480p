1f1cf26c21f843e193da6cc4dabfd89240e13302
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-12-27 17:11:58 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -12,15 +12,16 @@ loop = asyncio.get_event_loop()
 @app.on_message(filters.command(["help","ping"]))
 async def start(bot, message: Message):
   return await message.reply_text("⚡ **Bot Is up...**")
-
-async def start_bot():
+@app.on_message(filters.chat(-1001159872623) & filters.photo)    
+async def start_bot(bot, message):
+  post_id = message.message_id
   print("==================================")
   print("[INFO]: AutoAnimeBot Started Bot Successfully")
   print("==========JOIN @Latest_ongoing_airing_animes=========")
 
   print("[INFO]: Adding Parsing Task")
   asyncio.create_task(auto_parser())
-  asyncio.create_task(tg_handler())
+  asyncio.create_task(tg_handler(post_id))
   
   await idle()
   print("[INFO]: BOT STOPPED")
